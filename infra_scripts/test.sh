f8d9acb068955c9a371eb598ac6e42e7289ec736
Chao Cheng <chaocheng@college.harvard.edu>
2021-12-26 07:30:47 -0500

    Update test script

--- infra_scripts/test.sh
+++ infra_scripts/test.sh
@@ -16,22 +16,71 @@ RAND_SEED=42
 
 # create milestone 1 data
 cd project_tests/data_generation_scripts
-python milestone1.py $DATA_SIZE $RAND_SEED $ABS_TEST_DIR $ABS_TEST_DIR
+# python milestone1.py $DATA_SIZE $RAND_SEED $ABS_TEST_DIR $ABS_TEST_DIR
 
 # setup code
 cd ../../src
+rm -r data
 make clean
 make all
 
 # record results of tests
+# valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes
+
 echo ""
-echo "running test 1"
+echo "-----running test 1-----"
 ./server > $STUDENT_OUTPUT_DIR/test01gen.server.debug.out &
 sleep 1
 ./client < ../$REL_TEST_DIR/test01gen.dsl
+
 echo ""
-echo "running test 2"
+echo "-----running test 2-----"
 ./server > $STUDENT_OUTPUT_DIR/test02gen.server.debug.out &
 sleep 1
 ./client < ../$REL_TEST_DIR/test02gen.dsl
+
+echo ""
+echo "-----running test 3-----"
+./server > $STUDENT_OUTPUT_DIR/test03gen.server.debug.out &
+sleep 1
+./client < ../$REL_TEST_DIR/test03gen.dsl
+
+echo ""
+echo "-----running test 4-----"
+./server > $STUDENT_OUTPUT_DIR/test04gen.server.debug.out &
+sleep 1
+./client < ../$REL_TEST_DIR/test04gen.dsl
+
+echo ""
+echo "-----running test 5-----"
+./server > $STUDENT_OUTPUT_DIR/test05gen.server.debug.out &
+sleep 1
+./client < ../$REL_TEST_DIR/test05gen.dsl
+
+echo ""
+echo "-----running test 6-----"
+./server > $STUDENT_OUTPUT_DIR/test06gen.server.debug.out &
+sleep 1
+./client < ../$REL_TEST_DIR/test06gen.dsl
+
+echo ""
+echo "-----running test 7-----"
+./server > $STUDENT_OUTPUT_DIR/test07gen.server.debug.out &
+sleep 1
+./client < ../$REL_TEST_DIR/test07gen.dsl
+
+echo ""
+echo "-----running test 8-----"
+./server > $STUDENT_OUTPUT_DIR/test08gen.server.debug.out &
+sleep 1
+./client < ../$REL_TEST_DIR/test08gen.dsl
+
+echo ""
+echo "-----running test 9-----"
+./server > $STUDENT_OUTPUT_DIR/test09gen.server.debug.out &
+sleep 1
+./client < ../$REL_TEST_DIR/test09gen.dsl
+
+echo ""
+echo "-----killing server-----"
 if pgrep server; then pkill server; fi
